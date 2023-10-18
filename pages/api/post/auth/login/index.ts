09fe5449c0052ec25cc909a9df71993810354ce9
Ejayz <32452006+Ejayz@users.noreply.github.com>
2023-10-19 01:50:34 +0800

    Update index.ts

--- pages/api/post/auth/login/index.ts
+++ pages/api/post/auth/login/index.ts
@@ -1,6 +1,7 @@
 import { SupabaseClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next";
 import { serialize } from "cookie";
+const ipRequest = require("request-ip");
 const supabaseURL: string = process.env.SUPABASE_URL || "";
 const supabaseAnonKey: any = process.env.SUPABASE_ANON_KEY;
 const supabase = new SupabaseClient(supabaseURL, supabaseAnonKey);
@@ -8,13 +9,15 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const ip=ipRequest.getClientIp(req);
+  console.log(ip)
   const { email, password } = req.body;
   const { data, error } = await supabase.auth.signInWithPassword({
     email: email,
     password: password,
   });
 
-  console.log(data, error);
+
   if (error) {
     return res.status(400).json({ message: error.message });
   } else if (data.user?.identities?.length === 0) {
